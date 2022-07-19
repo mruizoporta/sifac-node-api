9427200db66cdddb7a3ba6c3dae7df5548e31776
mruizoporta <mruiz.oporta@gmail.com>
2022-07-18 23:10:41 -0600

    Prueba

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import catalogvalueRoutes from './routes/catalogvalue.js';
 import cityRoutes from './routes/city.js';
 import countryRoutes from './routes/country.js';
 
+//probando
 //Initialization
 const app = express();
 
